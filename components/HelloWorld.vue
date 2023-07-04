0db7f84e8aaaf0070dbb30df7932032d07bbe424
sebastiangentini <102331445+sebastiangentini@users.noreply.github.com>
2023-07-04 11:29:54 -0300

    Basic vue js layout

---
+++ components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Welcome to my portfolio!',
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #333;
+}
+</style>
---
+++ main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+new Vue({
+  el: '#app',
+  components: {
+    HelloWorld,
+  },
+  template: '<HelloWorld/>',
+});
